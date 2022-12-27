158f35fd2e6dc042896e2707c5a42b1e981d2573
pawelsznura <91138465+pawelsznura@users.noreply.github.com>
2022-12-27 15:51:35 +0000

    returns labels with > 0.5 score

--- main.py
+++ main.py
@@ -9,22 +9,57 @@ import img2text_models.facebook_regnet as facebook_regnet
 
 import img2text_models.nvidia_mit as nvidia_mit
 
+import json
+
+import numpy as np
+
 models = [microsoft_beit, microsoft_resnet, microsoft_swin,
         google_vit,
         facebook_convnext, facebook_regnet,
         nvidia_mit]
 
-responses=[]
 
 def main():
 
+    img = "insp_img/bonesaw.jpg"
+
+    responses = get_text_classification_responses(img)
+
+    print_text_classification_responses(responses)
+
+    print(get_best_classification(responses))
+    
+
+
+def print_text_classification_responses(responses):
+    for model, response in zip(models, responses):
+        print(model.__name__)
+        print(json.dumps(response, indent=4))
+
+
+def get_text_classification_responses(img):
+    responses=[]
     for model in models:
-        x = model.query("insp_img/sponge2.jpg")
+        x = model.query(img)    
+        # if error 503 than retry 
+
         responses.append(x)
 
-    # response  = google_vit.query("insp_img/sponge2.jpg")
+    return responses
 
-    print(responses)
+def get_best_classification(responses):
+    tokens = []
+    for model in responses:
+
+        for x in model:
+            # print("reponses x")
+            # print(x)
+            if x['score'] > 0.5:
+                tokens.append(x['label'].split(", "))
+
+    return np.unique(tokens)
+
+
 
 
 
