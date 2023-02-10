65775a6802c3bf699560afecdcd28f9a17babdf2
Pawel <p.sznura@rgu.ac.uk>
2023-02-10 13:46:57 +0000

    added prompt options

--- run.py
+++ run.py
@@ -11,6 +11,21 @@ all_img = os.listdir("insp_img/")
 # img = "insp_img/"+all_img[2]
 # img = "insp_img/elephant.jpg"
 
+# Prompt engineering 
+# how to keep everything in frame? 
+
+details_list = ["Hyper realistic", "Highly detailed", "high resolution", "detail", "8 k", "dslr", "cinematic lighting", "hyperdetailed"]
+fashion_list = ["concept", "fashion design", "elegant", "luxury",]
+
+designers_list = ["design by", "balenciaga", "laagam", "loewe", "aimeleondore", "Louis Vuitton"]
+
+front_prompt = ["A sketch of ", "A drawing of", "A photograph of", "good lighting", "ray tracing", "realistic" ]
+
+styles = ["Modernistic", "Abstract"]
+
+neg_prompt2 = "ugly, deformed, deformity, ugliness, blurry, disfigured, poorly drawn face, mutation, mutated, extra limbs, messy drawing, text, cropped head, cropped face"
+
+
 prompt_part = "female swimsuit design inspired by "
 neg_prompt = "ugly, tiling, poorly drawn, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, blurred, text, watermark, grainy"
 
