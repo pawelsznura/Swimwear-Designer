4c792e287da105b27020545b150349a0ab5e39fe
Pawel <p.sznura@rgu.ac.uk>
2023-03-20 18:39:51 +0000

    frechet inception distance

--- frechet_inception_distance.py
+++ frechet_inception_distance.py
@@ -1,43 +1,65 @@
 import torch
-# _ = torch.manual_seed(123)
+_ = torch.manual_seed(123)
 from torchmetrics.image.fid import FrechetInceptionDistance
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+from torchvision.transforms import Resize
 import os 
 from PIL import Image
 
 
-def frechet_inception_distance(gen_img_paths, real_img_paths):
+def frechet_inception_distance(gen, real):
     fid = FrechetInceptionDistance(feature=64)
     # feature - an integer will indicate the inceptionv3 
     # feature layer to choose. Can be one of the 
     # following: 64, 192, 768, 2048
+    fid.update(real, real=True)
+    fid.update(gen, real=False)
+    return fid.compute()
 
 
 def transform_to_tensor(img_paths):
     """takes as input array of image paths, returns array of tensor"""
-    transform = ToTensor()
+    # transform = ToTensor()
+
+    transform = transforms.Compose([
+    transforms.PILToTensor()
+    ])
+    resize = Resize((512, 512))
 
     tensors = []
 
     for img_path in img_paths:
         img = Image.open(img_path)
-        img_tensor = transform(img)
+        img_resized = resize(img)
+        img_tensor = transform(img_resized)
+        # img_tensor = img_tensor.to(torch.uint8)
         tensors.append(img_tensor)
 
-    return tensors
+    return torch.stack(tensors)
 
 def get_file_paths(directory):
     file_paths = []
     for file in os.listdir(directory):
         file_paths.append(directory+"/"+file)
 
-    # custom sorting function to sort the list based on the numerical value of the file names
-    file_paths.sort(key=lambda x: int(x.split('/')[-1].split('.')[0]))
+    if directory == "created_images":
+        # custom sorting function to sort the list based on the numerical value of the file names
+        file_paths.sort(key=lambda x: int(x.split('/')[-1].split('.')[0]))
     
     # print(file_paths)
     return file_paths
 
-img_paths = get_file_paths("created_images")
+gen_img_paths = get_file_paths("created_images")
+real_img_paths = get_file_paths("imagenet")
+# print(real_img_paths[-10:])
+# print(gen_img_paths[-100:])
+gen_tensor = transform_to_tensor(gen_img_paths)
+real_tensor = transform_to_tensor(real_img_paths)
+# print("\nREAL: \n\n")
+# print(real_tensor[:1])
+
+print(frechet_inception_distance(gen_tensor, real_tensor))
+
 
 
 
--- test.py
+++ test.py
@@ -1,52 +1,5 @@
-# File to do some run some quick code snippets
-import main 
-
-# import Stable_Diffusion.txt2img_gpu as onnx
-
-
-# prompt = "guy sitting in front of computer"
-
-
-
-# model_id = "stable_diffusion_onnx"    
-# pipe = onnx.onnxPipeline()
-
-
-# img_path = 'test.jpg'
-# neg_prompt = ""
-# image = pipe(prompt, negative_prompt=neg_prompt).images[0]  
-
-# image.save(img_path)
-
-
-# new_dir = main.get_new_file_name("created_images/")
-# print(new_dir)
-
-
 import torch
-from diffusers import StableDiffusionAttendAndExcitePipeline
 
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionAttendAndExcitePipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-# pipe = pipe.to("cuda")
+x=torch.randint(100, 255, (100, 3, 299, 299), dtype=torch.uint8)
 
-prompt = "a cat and a frog"
-
-# use get_indices function to find out indices of the tokens you want to alter
-pipe.get_indices(prompt)
-
-token_indices = [2, 5]
-seed = 6141
-generator = torch.manual_seed(seed)
-
-images = pipe(
-    prompt=prompt,
-    token_indices=token_indices,
-    guidance_scale=7.5,
-    generator=generator,
-    num_inference_steps=50,
-    max_iter_to_alter=25,
-).images
-
-image = images[0]
-image.save(f"../images/{prompt}_{seed}.png")
+print(type(x))
